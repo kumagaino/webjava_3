515e47557d03acdfc67f1778f89ea18e3776fb0d
systena <systena@PCK-138-279>
2019-09-29 18:20:23 +0900

    ディレクトリ作成

---
+++ src/main/java/jp/co/systena/tigerscave/lasttask/LastTaskApplication.java
@@ -0,0 +1,13 @@
+package jp.co.systena.tigerscave.lasttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastTaskApplication.class, args);
+	}
+
+}
